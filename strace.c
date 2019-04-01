69ae0dbf66594d56b684a8564e0dce77ba2cd38f
Eugene Syromyatnikov <evgsyr@gmail.com>
2019-04-01 03:13:37 +0200

    Add an option for raising Strauss awareness
    As Der Strauss is the strace mascot and also an endangered species,
    it is now strace's responsibility to raise awareness about it.
    
    * straus.c: New file.
    * straus.h: New header.
    * Makefile.am (strace_SOURCES): Add them.
    * strace.c: Include "straus.h".
    (version_verbosity): New static variable.
    (print_version): Add verbosity argument, call print_straus.
    (increase_version_verbosity): New function.
    (init) <case 'V'>: Call increase_version_verbosity.
    (init): Call print_version and exit if version_verbosity is non-zero.
    * tests/straus_body.exp: New file.
    * tests/straus_head.exp: Likewise.
    * tests/Makefile.am (EXTRA_DIST): Add them.
    * tests/strace-V.test: Update expected output, add checks.
    * strace.1.in: Document it.
    * NEWS: Mention it.

--- strace.c
+++ strace.c
@@ -37,6 +37,7 @@
 #include "ptrace_syscall_info.h"
 #include "scno.h"
 #include "printsiginfo.h"
+#include "straus.h"
 #include "trace_event.h"
 #include "xstring.h"
 #include "delay.h"
@@ -78,6 +79,8 @@ unsigned int qflag;
 static unsigned int tflag;
 static bool rflag;
 static bool print_pid_pfx;
+
+static unsigned int version_verbosity;
 
 /* -I n */
 enum {
@@ -196,7 +199,7 @@ strerror(int err_no)
 #endif /* HAVE_STERRROR */
 
 static void
-print_version(void)
+print_version(unsigned int verbosity)
 {
 	static const char features[] =
 #ifdef ENABLE_STACKTRACE
@@ -228,6 +231,9 @@ print_version(void)
 	       PACKAGE_NAME, PACKAGE_VERSION, COPYRIGHT_YEAR, PACKAGE_URL);
 	printf("\nOptional features enabled:%s\n",
 	       features[0] ? features : " (none)");
+
+	/* Raise straus awareness */
+	print_straus(verbosity);
 }
 
 static void
@@ -1547,6 +1553,13 @@ set_sighandler(int signo, void (*sighandler)(int), struct sigaction *oldact)
 	sigaction(signo, &sa, oldact);
 }
 
+static void
+increase_version_verbosity(void)
+{
+	if (version_verbosity < (STRAUS_START_VERBOSITY + straus_lines))
+		version_verbosity++;
+}
+
 /*
  * Initialization part of main() was eating much stack (~0.5k),
  * which was unused after init.
@@ -1691,8 +1704,7 @@ init(int argc, char *argv[])
 			qualify("abbrev=none");
 			break;
 		case 'V':
-			print_version();
-			exit(0);
+			increase_version_verbosity();
 			break;
 		case 'w':
 			count_wallclock = 1;
@@ -1720,6 +1732,11 @@ init(int argc, char *argv[])
 			error_msg_and_help(NULL);
 			break;
 		}
+	}
+
+	if (version_verbosity) {
+		print_version(version_verbosity);
+		exit(0);
 	}
 
 	argv += optind;
